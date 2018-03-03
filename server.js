fd9bcf360efb232dd6151d3e47b39444d4b5db69
Akinwale Habib <Akinwale Habib>
2018-03-03 23:03:08 +0100

    Refactored server.js. Corrected stylesheet href and main.js src attribute

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 /*eslint no-console: 1*/
+/*eslint no-undef: 1 */
 
 var express = require("express");
 var app = express();
-var thisProcess = require("process");
+app.use(express.static("public"));
+var process = require("process");
+var path = require("path");
 
 app.get("/", function(req, res) {
-    res.send("Hello World");
+    res.sendFile(path.join(__dirname + "/index.html"));
 });
-var port = thisProcess.argv[2] || 3000;
+var port = process.argv[2] || 3000;
 app.listen(port);
-console.log("app listening on port " + port);
