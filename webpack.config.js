1bfefb72cd4cc48f91831db536211902d2baac92
Akinwale Habib <akinwalehabib@hotmail.com>
2018-03-27 11:01:24 +0100

    Installed babel-core, babel-loader, react and es2015 babel presets. Ignore line ending types in eslint

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+module.exports = {
+    entry: "./client/index.js",
+    output: {
+        path: path.resolve("dist"),
+        filename: "index_bundle.js"
+    },
+    module: {
+        rules: [
+            { test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            }, {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            }
+        ]
+    }
+};
